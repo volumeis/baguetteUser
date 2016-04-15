8626ea6d12f40a75c723964af121c900582333ae
kim kyungchul <twinmoon77@gmail.com>
2016-04-15 20:57:06 +0900

    cart.js 수정

--- baguetteUser/src/main/webapp/js/cart.js
+++ baguetteUser/src/main/webapp/js/cart.js
@@ -1,21 +1,46 @@
-$(function(){
+$(function () {
+
+    $("#select-qty").change(function () {
+            var str = 0;
+
+            $("#select-qty option:selected").each(function () {
+                str += +($(this).val() * $("#price").text());
+            });
+
+            $("#money").text(str);
+
+        })
+        .change();
+
+    $("#select-qty1").change(function () {
+            var str = 0;
+
+            $("#select-qty1 option:selected").each(function () {
+                str += +($(this).val() * $("#price1").text());
+            });
+
+            $("#money1").text(str);
+
+        })
+        .change();
     
-     $("#select-qty").change(function(){
-                            var str ="";
-                            $("select option:selected").each(function(){
-//                                str += $(this).val()*$("#price").val();
-                                str += $(this).val()*$("#price").text();
-                            });
-                            $("#money").text(str);
-                            $("#totalmoney").text(str);
-                        })
-                       .change();
-    
-    $('#delect-cart').click(function() {
-                    $('#cartcon').remove();
-                });
-    
-    $('#add-cart').click(function() {
-                 $('#cartcon').append($('cartcon'));
-             });
+    $("select").change(function () {
+            var sum = 0;
+
+            sum += parseInt($("#money").text()) + parseInt($("#money1").text());
+            $("#totalmoney").text(sum);
+        })
+        .change();
+
+    $('#delect-cart').click(function () {
+        $('#cartcon').remove();
+    });
+
+    $('#add-cart').click(function () {
+        $('#cartcon').append($('cartcon'));
+    });
+});
+
+$(function () {
+
 });
