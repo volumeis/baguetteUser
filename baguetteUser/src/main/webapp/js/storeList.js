f3930e30054d903b5725566e34e9fd3b1bc890c5
MinhoYoo <uc9773@naver.com>
2016-04-18 18:01:22 +0900

    storeLiheader.css storeList.css storeList.html storeList.html storeList.js
    스토어 리스트 js,css,html 수정
    
    minho
    
    04.18.16

--- baguetteUser/src/main/webapp/js/storeList.js
+++ baguetteUser/src/main/webapp/js/storeList.js
@@ -1,54 +1,46 @@
+$(function () {
 
-$(function(){
-    
     var index = 0;
     var i = 0;
     var j = 0;
-    $(window).scroll(function() {
-        
+    $(document).scrollstop(function () {
         if ($(window).scrollTop() == $(document).height() - $(window).height()) {
             i++;
             j++;
             index++;
-            
-//            var address = 'address'+ index;
-//            var storename = 'storename' + index;
-            
-            var address =  $(document.createElement('div')).addClass('address');
-            address.text("address : "+index);
-            var name =  $(document.createElement('div')).addClass('name');
-            name.text("name : "+index);
-            var caption = $(document.createElement('div')).addClass('caption');
-            caption.html(address.add(name) );
-            var storeBanner = $(document.createElement('img')).attr({
-                src : "../image/store03.jpg",
-                alt : "alt : " + index
-            });           
-            
-            var store = $(document.createElement('div')).addClass('storeListImageDiv').html(storeBanner.add(caption));
-            var storeContainer =$(document.createElement('div')).addClass('col-xs-12').html(store);
-            
-            var zzz = '<div id="st'+j+'" style="display: none;" ><img src="../image/store0'+i+'.jpg"> </div>';
-            
-            $('#storeListDiv').append(storeContainer)
-                $('#st'+j).fadeIn(1000,function(){
-                    
-                });
-            $(window).scrollTop( $(document).height() - $(window).height()- 1);
-//            alert("1 :" + $(window).height()+ "\n"+
-//                  "2 :" + $(document).height());
-//            
-            
+            var address = 'address' + index;
+            var storename = 'storename' + index;
+            var address = $(document.createElement('div')).addClass('address');
+            address.text("address : " + index);
+            $(".ui-grid-solo").append(address);
+            var name = $(document.createElement('div')).addClass('name');
+            name.text("name : " + index);
+            var figcaption = $(document.createElement('figcaption'));
+            figcaption.html(address.add(name));
 
-            if(i==3){
-                i=0;
+            var img =
+                $(document.createElement('img')).attr({
+                    src: "../image/store03.jpg",
+                    alt: "alt : " + index
+                });
+
+            var figuere = $(document.createElement('figure')).html(img.add(figcaption));
+
+            var storeLink =
+                $(document.createElement('a')).attr({
+                    href: "storeHomeTest.html",
+                    class : "ui-link"
+                });
+
+            var storeBlockA = $(document.createElement('div')).addClass('ui-block-a').html(storeLink.html(figuere));
+
+            $('#list-store').append(storeBlockA);
+            $('#st' + j).fadeIn(1000, function () {});
+            $(window).scrollTop($(document).height() - $(window).height() - 1);
+
+            if (i == 3) {
+                i = 0;
             }
-//                       
-//            
         }
     });
-//    
-//    $( ".storeListImageDiv" ).first().show( "fast", function showNext() {
-//    $( this ).next( ".storeListImageDiv" ).show( "fast", showNext );
-//  });
 });
