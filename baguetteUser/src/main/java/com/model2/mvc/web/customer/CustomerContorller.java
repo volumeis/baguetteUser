7e6afb894d8f9e3859c93a5590483a1c177a0402
kim kyungchul <twinmoon77@gmail.com>
2016-04-19 09:20:29 +0900

    로그인 컨트롤러 구현!

--- baguetteUser/src/main/java/com/model2/mvc/web/customer/CustomerContorller.java
+++ baguetteUser/src/main/java/com/model2/mvc/web/customer/CustomerContorller.java
@@ -1,5 +1,6 @@
 package com.model2.mvc.web.customer;
 
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,56 +9,90 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.model2.mvc.service.customer.CustomerService;
 import com.model2.mvc.service.domain.Customer;
-
-
 
 //==> Customer관리 Controller
 @Controller
 @RequestMapping("/customer/*")
 public class CustomerContorller {
-	
-	///Field
+
+	/// Field
 	@Autowired
 	@Qualifier("customerServiceImpl")
 	private CustomerService customerService;
 
-	public CustomerContorller(){
+	public CustomerContorller() {
 		System.out.println(this.getClass());
 	}
-	
-	@RequestMapping( value="addCustomer", method=RequestMethod.POST)
-	public String addCustomer( @ModelAttribute("customer") Customer customer ) throws Exception {
 
-		System.out.println("/customer/addCustomer : POST");
-	
-		customerService.addCustomer(customer);
-		
-		return "redirect:/customer/가입.jsp";
+	@RequestMapping(value = "jsonLogin", method = RequestMethod.POST)
+	public void jsonLogin(@RequestBody Customer customer, HttpSession session, Model model) throws Exception {
+
+		System.out.println("/customer/jsonLogin : POST");
+		// Business Logic
+		System.out.println("::" + customer);
+		Customer dbUser = customerService.getCustomer(customer.getCustomerTel());
+
+		if (dbUser != null && customer.getPassword().equals(dbUser.getPassword())) {
+
+			model.addAttribute("customer", dbUser);
+		} else {
+			model.addAttribute("customer", null);
+		}
 	}
-	//===========================================
-	//===========================================
-	@RequestMapping( value="getJsonCustomer/{customerTel}", method=RequestMethod.GET)
-	public void getJsonCustomer(	@PathVariable String customerTel,
-		HttpSession ssesion,	Model model	) throws Exception{
-		System.out.println("/getJsonCustomer/getUser : GET" + customerTel);
-		//Business Logic
+
+	@RequestMapping(value = "addCustomer", method = RequestMethod.POST)
+	public void addCustomer(@RequestBody Customer customer, HttpServlet request) throws Exception {
+
+		// String customerTel =request.getInitParameter("customerTel");
+		System.out.println("/customer/addCustomer : POST");
+
+		customerService.addCustomer(customer);
+
+	}
+
+	// ===========================================
+	// ===========================================
+	@RequestMapping(value = "getCustomer", method = RequestMethod.GET)
+	public void getCustomer(@RequestParam("customerTel") String customerTel, Model model) throws Exception {
+
+		System.out.println("/user/getUser : GET");
+		// Business Logic
 		Customer customer = customerService.getCustomer(customerTel);
-		
+		// Model 과 View 연결
+		model.addAttribute("customer", customer);
+
+	}
+
+	@RequestMapping(value = "getJsonCustomer/{customerTel}", method = RequestMethod.GET)
+	public void getJsonCustomer(@PathVariable String customerTel, HttpSession ssesion, Model model) throws Exception {
+		System.out.println("/getJsonCustomer/getUser : GET" + customerTel);
+		// Business Logic
+		Customer customer = customerService.getCustomer(customerTel);
+
 		System.out.println();
 		// Model 과 View 연결
-		model.addAttribute("customer",customer);
+		model.addAttribute("customer", customer);
 	}
-	//===========================================
-	//===========================================
-/*	@RequestMapping( value="test", method=RequestMethod.GET)
-	public String getJsonCustomer() throws Exception{
-		System.out.println("/getJsonCustomer/getUser : GET");
-		return "/yhh.jsp";
-	}*/
-	
+
+	// ===========================================
+	// ===========================================
+	@RequestMapping(value = "checkDuplication", method = RequestMethod.POST)
+	public void checkDuplication(@RequestParam("customerTel") String customerTel, Model model) throws Exception {
+
+		System.out.println("/customer/checkDuplication : POST");
+		// Business Logic
+		boolean result = customerService.checkDuplication(customerTel);
+		// Model 과 View 연결
+		model.addAttribute("result", new Boolean(result));
+		model.addAttribute("customer", customerTel);
+
+	}
+
 }
