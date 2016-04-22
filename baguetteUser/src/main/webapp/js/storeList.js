92ec21ff0a5d44601007ce8fe5de9fe3d5ea2061
MinhoYoo <uc9773@naver.com>
2016-04-22 13:27:27 +0900

    loader 기능 추가
    민호
    
    04.20.16

--- baguetteUser/src/main/webapp/js/storeList.js
+++ baguetteUser/src/main/webapp/js/storeList.js
@@ -1,49 +1,49 @@
 $(function () {
-
-    var index = 0;
-    var i = 0;
-    var j = 0;
-    $(document).scrollstop(function () {
-        if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-            i++;
-            j++;
-            index++;
-            var address = 'address' + index;
-            var storename = 'storename' + index;
-            var address = $(document.createElement('div')).addClass('address');
-            address.text("address : " + index);
-            $(".ui-grid-solo").append(address);
-            var name = $(document.createElement('div')).addClass('name');
-            name.text("name : " + index);
-            var figcaption = $(document.createElement('figcaption'));
-            figcaption.html(address.add(name));
-
-            var img =
-                $(document.createElement('img')).attr({
-                    src: "../image/store03.jpg",
-                    alt: "alt : " + index
-                });
-
-            var figuere = $(document.createElement('figure')).html(img.add(figcaption));
-
-            var storeLink =
-                $(document.createElement('a')).attr({
-                    href: "storeHome.html",
-                    class: "ui-link"
-                });
-
-            var storeBlockA = $(document.createElement('div')).addClass('ui-block-a').html(storeLink.html(figuere));
-
-            $('#list-store').append(storeBlockA);
-            $('#st' + j).fadeIn(1000, function () {});
-            $(window).scrollTop($(document).height() - $(window).height() - 1);
-
-            //http://java78bit404.iptime.org:8025/image/storeImg/artisee_sansungtown.jpg
-            if (i == 3) {
-                i = 0;
-            }
-        }
-    });
+//
+//    var index = 0;
+//    var i = 0;
+//    var j = 0;
+//    $(document).scrollstop(function () {
+//        if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+//            i++;
+//            j++;
+//            index++;
+//            var address = 'address' + index;
+//            var storename = 'storename' + index;
+//            var address = $(document.createElement('div')).addClass('address');
+//            address.text("address : " + index);
+//            $(".ui-grid-solo").append(address);
+//            var name = $(document.createElement('div')).addClass('name');
+//            name.text("name : " + index);
+//            var figcaption = $(document.createElement('figcaption'));
+//            figcaption.html(address.add(name));
+//
+//            var img =
+//                $(document.createElement('img')).attr({
+//                    src: "../image/store03.jpg",
+//                    alt: "alt : " + index
+//                });
+//
+//            var figuere = $(document.createElement('figure')).html(img.add(figcaption));
+//
+//            var storeLink =
+//                $(document.createElement('a')).attr({
+//                    href: "storeHome.html",
+//                    class: "ui-link"
+//                });
+//
+//            var storeBlockA = $(document.createElement('div')).addClass('ui-block-a').html(storeLink.html(figuere));
+//
+//            $('#list-store').append(storeBlockA);
+//            $('#st' + j).fadeIn(1000, function () {});
+//            $(window).scrollTop($(document).height() - $(window).height() - 1);
+//
+//            //http://java78bit404.iptime.org:8025/image/storeImg/artisee_sansungtown.jpg
+//            if (i == 3) {
+//                i = 0;
+//            }
+//        }
+//    });
 
     if ($(window).scrollTop() == 0 || ($(window).height() >= $(document).height())) {
 //        alert("123");
@@ -53,9 +53,8 @@ $(function () {
 
 
     //    var storeAddress = "강남구";
-    $(document).one("pagebeforecreate", function () {
-
-
+    $(document).one("pageinit", function () {
+        console.log('pageinit : storeList.js');
         $.ajax({
             url: COMMONWEBSERVER + "/store/getJsonStoreList/" + "서울",
             /*encodeURIComponent(storeAddress),*/
@@ -97,6 +96,7 @@ $(function () {
                     var storeBlockA = $(document.createElement('div')).addClass('ui-block-a').html(storeLink.html(figuere));
 
                     $('#list-store').append(storeBlockA);
+                    
                 });
 
             },
