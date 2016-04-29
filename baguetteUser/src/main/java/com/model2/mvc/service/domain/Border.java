da720694a31d7d6666223610e47223f4f382bfbc
kim kyungchul <twinmoon77@gmail.com>
2016-04-29 17:58:23 +0900

    Border  04.29.16 서형섭

--- baguetteUser/src/main/java/com/model2/mvc/service/domain/Border.java
+++ baguetteUser/src/main/java/com/model2/mvc/service/domain/Border.java
@@ -10,72 +10,77 @@ public class Border {
 	private int ono; // 빵주문 일련번호
 	private String otran; // 주문상태
 	private Date odate; // 주문일
-
-	private int customerNo; // 고객번호
+   	private int customerNo; // 고객번호
 	private String or_chk; // 리뷰여부
-
-	private int breadNo;
-
+    private int breadNo;
+    private String name;
+    private int price;
+    private String img;
 	public int getOqty() {
 		return oqty;
 	}
-
 	public void setOqty(int oqty) {
 		this.oqty = oqty;
 	}
-
 	public int getOno() {
 		return ono;
 	}
-
 	public void setOno(int ono) {
 		this.ono = ono;
 	}
-
 	public String getOtran() {
 		return otran;
 	}
-
 	public void setOtran(String otran) {
 		this.otran = otran;
 	}
-
 	public Date getOdate() {
 		return odate;
 	}
-
 	public void setOdate(Date odate) {
 		this.odate = odate;
 	}
-
 	public int getCustomerNo() {
 		return customerNo;
 	}
-
 	public void setCustomerNo(int customerNo) {
 		this.customerNo = customerNo;
 	}
-
 	public String getOr_chk() {
 		return or_chk;
 	}
-
 	public void setOr_chk(String or_chk) {
 		this.or_chk = or_chk;
 	}
-
 	public int getBreadNo() {
 		return breadNo;
 	}
-
 	public void setBreadNo(int breadNo) {
 		this.breadNo = breadNo;
 	}
-
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
 	@Override
 	public String toString() {
 		return "Border [oqty=" + oqty + ", ono=" + ono + ", otran=" + otran + ", odate=" + odate + ", customerNo="
-				+ customerNo + ", or_chk=" + or_chk + ", breadNo=" + breadNo + "]";
+				+ customerNo + ", or_chk=" + or_chk + ", breadNo=" + breadNo + ", name=" + name + ", price=" + price
+				+ ", img=" + img + "]";
 	}
-
+	
 }
