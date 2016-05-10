88c4787769aaf64d3d65584ba16d6225cf9a1634
MinhoYoo <uc9773@naver.com>
2016-05-10 20:15:23 +0900

    .

--- baguetteUser/src/main/java/com/model2/mvc/common/aspect/LogAspectJ.java
+++ baguetteUser/src/main/java/com/model2/mvc/common/aspect/LogAspectJ.java
@@ -9,14 +9,13 @@ public class LogAspectJ {
 	public LogAspectJ() {
 		System.out.println("\nCommon :: "+this.getClass()+"\n");
 	}
+	public static int LOGGED_NO = 0;
 	
 	//Around  Advice
 	public Object invoke(ProceedingJoinPoint joinPoint) throws Throwable {
 			
-		System.out.println("");
-		System.out.println("[Around before]"+
-													joinPoint.getTarget().getClass().getName() +"."+
-													joinPoint.getSignature().getName());
+		System.out.println("---LOGGING NO : " + LOGGED_NO++ + "--- --- --- --- --- --- ");
+		System.out.println("[Around before]"+ joinPoint.getTarget().getClass().getName() +"."+	joinPoint.getSignature().getName());
 		if(joinPoint.getArgs().length !=0){
 			System.out.println("[Around before]"+ joinPoint.getArgs()[0]);
 		}
