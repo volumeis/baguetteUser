e1f7d348f87ee5c93a6c35526b21490af3b0ccd6
MinhoYoo <uc9773@naver.com>
2016-05-18 16:38:08 +0900

    loader 삭
    민호
    
    05.19.16

--- baguetteUser/src/main/webapp/js/pages/storeList.js
+++ baguetteUser/src/main/webapp/js/pages/storeList.js
@@ -1,13 +1,4 @@
-$("#fakeloader").fakeLoader({
-    timeToHide: 4000, //Time in milliseconds for fakeLoader disappear
-    zIndex: "9999", //Default zIndex
-    spinner: "spinner1" //Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
-        //    bgColor: "transparent"
-});
-
 $(function () {
-    //$(document).one("pageshow", function () {
-
     $.ajax({
         url: COMMONWEBSERVER + "/store/getStoreListShort/" + "서울 서초구", //
         method: "GET",
@@ -39,11 +30,6 @@ $(function () {
 
                 var storeLink =
                     $(document.createElement('a')).attr({
-                        //  href: "./storeHome.html?storeNo=2001"
-                        //  href: "./storeHome.html?storeNo="+store.storeNo
-                        //  href: "../html-src/storeHome.html?storeNo=" + store.storeNo,
-                        //  rel: "external",
-                        //  class: "ui-link"
                         id: store.storeNo
                     });
                 var storeBlockA = $(document.createElement('div')).addClass('ui-block-a').html(storeLink.html(figuere));
@@ -53,12 +39,12 @@ $(function () {
         complete: function () {
             console.log('complete : storeList ajax');
             callStoreHome();
-
-            $('#codeError').modal();
-
+            $('#MyPosDlg').modal();
+            
         },
         error: function (JSONData, status) {
-            //                alert("잘못된 요청입니다.");
+            console.log('error : storeList ajax');
+            console.log(status);
         }
     });
 });
